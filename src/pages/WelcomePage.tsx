f9f9d623e99f11211e9c896baee06a25ab49ae7d
Flaviodscy <gorodscyflavio@gmail.com>
2025-07-17 15:00:31 -0400

    WelcomePage.tsx

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -4,13 +4,8 @@ import { ArrowRightIcon, RulerIcon, StarIcon, CheckIcon, SparklesIcon, ShieldChe
 import { useNavigate } from 'react-router-dom';
 
 // --- iOS Install Banner Helpers ---
-function isIOS() {
-  return /iphone|ipad|ipod/i.test(window.navigator.userAgent);
-}
-
 function IOSInstallBanner() {
   // REMOVE the device check for now, always show the banner!
-  // if (!isIOS()) return null;
   return (
     <div style={{
       background: "#000",
@@ -64,39 +59,3 @@ export const WelcomePage = ({
   const featuredProducts = [{
     id: 1,
     name: 'Cloud Comfort Elite',
-    image: 'https://www.silkandsnow.com/wp-content/themes/silkandsnow/img/waffle_sham_eucalyptus_1.jpg',
-    price: '$129.99',
-    rating: 4.8,
-    reviews: 1250,
-    tag: 'Best Seller'
-  }, {
-    id: 2,
-    name: 'Memory Foam Contour',
-    image: 'https://m.media-amazon.com/images/I/51mjjfaTOAL._AC_SL1500_.jpg',
-    price: '$159.99',
-    rating: 4.9,
-    reviews: 890,
-    tag: "Editor's Choice"
-  }, {
-    id: 3,
-    name: 'Organic Cotton Deluxe',
-    image: 'https://hush.ca/cdn/shop/files/AUG28_00067crop_dbe51315-fc88-4394-a635-97b2347f2511.jpg?v=1736960335&width=1300',
-    price: '$119.99',
-    rating: 4.7,
-    reviews: 760,
-    tag: 'Eco-Friendly'
-  }];
-  const handleViewProductDetails = productId => {
-    navigate(`/product?id=${productId}`);
-  };
-
-  return (
-    <>
-      <IOSInstallBanner />
-      <motion.div initial="initial" animate="animate" variants={stagger} className="w-full">
-        {/* ...rest of your content... */}
-        {/* Hero Section, How It Works, etc. */}
-      </motion.div>
-    </>
-  );
-};
